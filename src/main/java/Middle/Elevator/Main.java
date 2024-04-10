32c6f29ea59496e3960ef087df984c0c7334c933
Pavel Davydov <Davydov.works@gmail.com>
2024-04-10 03:39:59 -0400

    add  Middle Task Elevator

--- src/main/java/Middle/Elevator/Main.java
+++ src/main/java/Middle/Elevator/Main.java
@@ -4,11 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Elevator elevator = new Elevator();
 
-        Cargo cargo1 = new Cargo("человек", 85);
+        Cargo cargo1 = new Cargo("Мужчина", 85);
         Cargo cargo2 = new Cargo("Диван", 125);
-        Cargo cargo3 = new Cargo("человек", 105);
+        Cargo cargo3 = new Cargo("Женщина", 105);
         Cargo cargo4 = new Cargo("Шкаф", 56);
-        Cargo cargo5 = new Cargo("Пианино", 250);
+        Cargo cargo5 = new Cargo("ребенок", 30);
+        Cargo cargo6 = new Cargo("Собака", 20);
 
         elevator.add(cargo1);
         elevator.add(cargo2);
@@ -21,6 +22,10 @@ public class Main {
 
         elevator.add(cargo3);
         elevator.add(cargo4);
+        elevator.add(cargo5);
+        elevator.add(cargo6);
+
+
 
         elevator.chekWeight();
     }
